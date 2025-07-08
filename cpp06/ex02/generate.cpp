4b3a45bd7024a2a305a1e71dad6497b7f88eca0b
42derr <dfasius@c1r7s5.42singapore.sg>
2025-07-08 17:25:15 +0800

    ok

--- cpp06/ex02/generate.cpp
+++ cpp06/ex02/generate.cpp
@@ -31,13 +31,27 @@ void identify(Base* p)
 
 void identify(Base& p)
 {
-    if (dynamic_cast<A*>(&p))
+    try {
+        (void)dynamic_cast<A&>(p);
         std::cout << "A\n";
-    else if (dynamic_cast<B*>(&p))
+        return ;
+    }
+    catch (std::exception& e) {}
+
+    try {
+        (void)dynamic_cast<B&>(p);
         std::cout << "B\n";
-    else if (dynamic_cast<C*>(&p))
+        return ;
+    }
+    catch (std::exception& e) {}
+
+    try {
+        (void)dynamic_cast<C&>(p);
         std::cout << "C\n";
-    else
-        std::cout << "Unknown\n";
+        return ;
+    }
+    catch (std::exception& e) {}
+
+    std::cout << "Unknown\n";
 }
 
